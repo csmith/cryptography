4aa4db83ef7c26bedc6f545775064d31fb4db257
Chris Smith <chris@chameth.com>
2021-05-22 00:04:32 +0100

    Add keyed caesar shift and A1Z26 encode

--- substitution.go
+++ substitution.go
@@ -5,9 +5,16 @@ import "strings"
 // CaesarShift performs a caesar shift of the given amount on all a-z/A-Z characters. All other characters are
 // left intact.
 func CaesarShift(input []byte, count uint8) []byte {
+	return CaesarShiftKeyed(input, []uint8{count})
+}
+
+// CaesarShiftKeyed performs a caesar shift on all a-z/A-Z characters. The amount of the shift can vary for each
+// character and is provided in the counts array. If the counts array is shorter than the input array, it will be
+// repeated. All other characters are left intact.
+func CaesarShiftKeyed(input []byte, counts []uint8) []byte {
 	builder := strings.Builder{}
 
-	shift := func(c, min byte) byte {
+	shift := func(c, min byte, count uint8) byte {
 		res := c - min
 		for res < min {
 			res += 26
@@ -15,12 +22,15 @@ func CaesarShift(input []byte, count uint8) []byte {
 		return min + (res+count)%26
 	}
 
+	ops := 0
 	for i := range input {
 		c := input[i]
 		if c >= 'a' && c <= 'z' {
-			builder.WriteByte(shift(c, 'a'))
+			builder.WriteByte(shift(c, 'a', counts[ops % len(counts)]))
+			ops++
 		} else if c >= 'A' && c <= 'Z' {
-			builder.WriteByte(shift(c, 'A'))
+			builder.WriteByte(shift(c, 'A', counts[ops % len(counts)]))
+			ops++
 		} else {
 			builder.WriteByte(c)
 		}
@@ -34,6 +44,21 @@ func CaesarShifts(input []byte) [26][]byte {
 	res[0] = input
 	for i := 1; i <= 25; i++ {
 		res[i] = CaesarShift(input, uint8(i))
+	}
+	return res
+}
+
+// A1Z26Encode replaces each letter of the alphabet with its corresponding ordinal, from A=1 to Z=26. Non-alphabet
+// characters are ignored.
+func A1Z26Encode(input []byte) []uint8 {
+	var res []uint8
+	for i := range input {
+		c := input[i]
+		if c >= 'a' && c <= 'z' {
+			res = append(res, 1 + c - 'a')
+		} else if c >= 'A' && c <= 'Z' {
+			res = append(res, 1 + c - 'A')
+		}
 	}
 	return res
 }
--- substitution_test.go
+++ substitution_test.go
@@ -24,7 +24,33 @@ func TestCaesarShift(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := CaesarShift([]byte(tt.query), uint8(tt.count)); !reflect.DeepEqual(got, []byte(tt.want)) {
-				t.Errorf("CaesarShift() = %v, want %v", got, tt.want)
+				t.Errorf("CaesarShift() = %s, want %s", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCaesarShiftKeyed(t *testing.T) {
+	tests := []struct {
+		name   string
+		query  string
+		counts []int
+		want   string
+	}{
+		{"empty", "", []int{1, 2, 3}, ""},
+		{"no rotation", "abcde", []int{0, 0}, "abcde"},
+		{"other chars", "_#A#/B-C", []int{1}, "_#B#/C-D"},
+		{"alternating", "Btusb jodmjobou, tfe opo pcmjhbou", []int{1, 2}, "Cvvuc lpfnlpdpw, uhf qqq qenlidpw"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			counts := make([]uint8, len(tt.counts))
+			for i := range tt.counts {
+				counts[i] = uint8(tt.counts[i])
+			}
+			if got := CaesarShiftKeyed([]byte(tt.query), counts); !reflect.DeepEqual(got, []byte(tt.want)) {
+				t.Errorf("CaesarShiftKeyed() = %s, want %s", got, tt.want)
 			}
 		})
 	}
@@ -37,7 +63,33 @@ func TestCaesarShifts(t *testing.T) {
 	for i := 0; i < 26; i++ {
 		want := CaesarShift(input, uint8(i))
 		if !reflect.DeepEqual(output[i], want) {
-			t.Errorf("CaesarShift()[0] = %v, want %v", output[0], want)
+			t.Errorf("CaesarShift()[0] = %s, want %s", output[0], want)
 		}
+	}
+}
+
+func TestA1Z26Encode(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []int
+	}{
+		{"empty", "", nil},
+		{"non a-z chars", "_#=!", nil},
+		{"lowercase", "abc-xyz", []int{1, 2, 3, 24, 25, 26}},
+		{"uppercase", "ABC-XYZ", []int{1, 2, 3, 24, 25, 26}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var want []uint8
+			for i := range tt.want {
+				want = append(want, uint8(tt.want[i]))
+			}
+
+			if got := A1Z26Encode([]byte(tt.input)); !reflect.DeepEqual(got, want) {
+				t.Errorf("A1Z26Encode() = %#v, want %#v", got, want)
+			}
+		})
 	}
 }
