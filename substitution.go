1a29054aa4220f822a64b4c7f228d89f807ae9c5
Chris Smith <chris@chameth.com>
2025-06-05 11:41:58 +0100

    Add A1Z26 operations

--- substitution.go
+++ substitution.go
@@ -1,6 +1,8 @@
 package cryptography
 
-import "strings"
+import (
+	"strings"
+)
 
 // CaesarShift performs a caesar shift of the given amount on all a-z/A-Z characters. All other characters are
 // left intact.
@@ -26,10 +28,10 @@ func CaesarShiftKeyed(input []byte, counts []uint8) []byte {
 	for i := range input {
 		c := input[i]
 		if c >= 'a' && c <= 'z' {
-			builder.WriteByte(shift(c, 'a', counts[ops % len(counts)]))
+			builder.WriteByte(shift(c, 'a', counts[ops%len(counts)]))
 			ops++
 		} else if c >= 'A' && c <= 'Z' {
-			builder.WriteByte(shift(c, 'A', counts[ops % len(counts)]))
+			builder.WriteByte(shift(c, 'A', counts[ops%len(counts)]))
 			ops++
 		} else {
 			builder.WriteByte(c)
@@ -55,10 +57,66 @@ func A1Z26Encode(input []byte) []uint8 {
 	for i := range input {
 		c := input[i]
 		if c >= 'a' && c <= 'z' {
-			res = append(res, 1 + c - 'a')
+			res = append(res, 1+c-'a')
 		} else if c >= 'A' && c <= 'Z' {
-			res = append(res, 1 + c - 'A')
+			res = append(res, 1+c-'A')
 		}
 	}
+	return res
+}
+
+// A1Z26Decode replaces each number with the corresponding letter of the alphabet, from A=1 to Z=26. Numbers outside
+// this range are ignored.
+func A1Z26Decode(input []uint8) []byte {
+	var res []byte
+	for i := range input {
+		c := input[i]
+		if c >= 1 && c <= 26 {
+			res = append(res, c+'A'-1)
+		}
+	}
+	return res
+}
+
+// A1Z26Parse parses the input, looking for numbers that could be decoded by A1Z26Decode, and inserts the result of
+// that operation in their place.
+func A1Z26Parse(input []byte) []byte {
+	var res []byte
+	var number []byte
+
+	processNumbers := func() {
+		defer func() {
+			number = nil
+		}()
+
+		if len(number) > 0 && len(number) <= 2 {
+			var value uint8
+			if len(number) == 2 {
+				value = 10 * (number[0] - '0') + (number[1] - '0')
+			} else {
+				value = number[0] - '0'
+			}
+
+			if decoded := A1Z26Decode([]uint8{value}); len(decoded) == 1 {
+				res = append(res, decoded[0])
+				return
+			}
+		}
+
+		res = append(res, number...)
+	}
+
+	for i := range input {
+		c := input[i]
+		if c >= '0' && c <= '9' {
+			number = append(number, c)
+		} else {
+			processNumbers()
+			number = nil
+			res = append(res, c)
+		}
+	}
+
+	processNumbers()
 	return res
 }
--- substitution_test.go
+++ substitution_test.go
@@ -93,3 +93,49 @@ func TestA1Z26Encode(t *testing.T) {
 		})
 	}
 }
+
+func TestA1Z26Decode(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []uint8
+		want  []byte
+	}{
+		{"empty", nil, nil},
+		{"out of range", []uint8{0, 27, 43, 99}, nil},
+		{"simple", []uint8{1, 2, 3, 24, 25, 26}, []byte("ABCXYZ")},
+		{"mixed out of range", []uint8{0, 1, 2, 3, 99, 24, 25, 26, 27}, []byte("ABCXYZ")},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := A1Z26Decode(tt.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("A1Z26Decode() = %#v, want %#v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestA1Z26Parse(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []byte
+		want  []byte
+	}{
+		{"empty", nil, nil},
+		{"non-numbers", []byte("This is a test"), []byte("This is a test")},
+		{"invalid numbers", []byte("0 000 100 300"), []byte("0 000 100 300")},
+		{"mixed numbers", []byte("0 1 000 3 10 100 4 300"), []byte("0 A 000 C J 100 D 300")},
+		{"mixed content", []byte("0.1-33_2a2 1"), []byte("0.A-33_BaB A")},
+		{"single valid number", []byte("3"), []byte("C")},
+		{"single multi-digit valid number", []byte("26"), []byte("Z")},
+		{"single invalid number", []byte("260"), []byte("260")},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := A1Z26Parse(tt.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("A1Z26Parse() = %s, want %s", got, tt.want)
+			}
+		})
+	}
+}
